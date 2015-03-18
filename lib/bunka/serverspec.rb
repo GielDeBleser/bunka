55075c4898aae60109b1f409fab8d8842ab24b48
Giel De Bleser <gieldebleser@gmail.com>
2015-03-18 10:53:32 +0100

    WIP_final_tests

--- lib/bunka/serverspec.rb
+++ lib/bunka/serverspec.rb
@@ -2,83 +2,85 @@ require 'rspec/core/rake_task'
 require 'rake'
 #require 'bunka/serverspecprinter'
 require 'rspec'
-require 'pry'
 require 'colorize'
 
 class Bunka
 	class << self
 
 		def serverspecsetup
+			
 			if File.exist?(ENV['HOME']+'/servers/sandboxservers')
 			@hosts = File.readlines(ENV['HOME']+'/servers/sandboxservers').each {|l| l.chomp!}
 			else
-			puts 'Serverfile not found'
-			exit
+				puts 'Serverfile not found'
+				exit
 			end
-  		@errors = []
-			#hosts.each do |host|
-    	#	short_name = host.split('.')[0]		
+  		
+			@errors = []
+			@status = []
+			@failed = 0
+			@passed = 0
+			@pending = 0
+			
+			RSpec.configure { |c| c.output_stream = nil }		
 
-				@hosts.each do |host|
+			@hosts.each do |host|
 				
-				config = RSpec.configuration
+			config = RSpec.configuration
 
-				formatter = RSpec::Core::Formatters::JsonFormatter.new(config.output_stream)
+			formatter = RSpec::Core::Formatters::JsonFormatter.new(config.output_stream)
 
-				# create reporter with json formatter
-				reporter =  RSpec::Core::Reporter.new(config)
-				config.instance_variable_set(:@reporter, reporter)
+			# create reporter with json formatter
+			reporter =  RSpec::Core::Reporter.new(config)
+			config.instance_variable_set(:@reporter, reporter)
 
-				# internal hack
-				# api may not be stable, make sure lock down Rspec version
-				loader = config.send(:formatter_loader)
-				notifications = loader.send(:notifications_for, RSpec::Core::Formatters::JsonFormatter)
+			# internal hack
+			# api may not be stable, make sure lock down Rspec version
+			loader = config.send(:formatter_loader)
+			notifications = loader.send(:notifications_for, RSpec::Core::Formatters::JsonFormatter)
 
-				reporter.register_listener(formatter, *notifications)
+			reporter.register_listener(formatter, *notifications)
     		
-				RSpec::Core::RakeTask.new('spec:'+host) do |t|
-      		t.pattern = ENV['HOME']+ @serverspecfile
-					t.fail_on_error = false
-					t.verbose = false
-				end
-					ENV['TARGET_HOST'] = host
-					begin
-  				#	Rake::Task['spec:'+ host].execute
-					
-				RSpec::Core::Runner.run([ENV['HOME'] + @serverspecfile])
-					
-						rescue RuntimeError
-  						puts 'Serverspec failed'
-					end
-				@h = formatter.output_hash
+			ENV['TARGET_HOST'] = host
+			begin
+  			#	Rake::Task['spec:'+ host].execute
+				RSpec::Core::Runner.run([ENV['HOME'] + @serverspecfile])	
+			rescue RuntimeError
+  			puts 'Serverspec failed'
+			end
 				
-				binding.pry
-				
-				@h[:examples].each do |x|
-				if x[:status] == "failed"
-				@errors << (host + ':  ' + x[:full_description].red)
+			@h = formatter.output_hash
+			@h[:examples].each do |x|
+				if x[:status] == 'failed'
+					@errors << (host + ':  ' + x[:full_description].red)
 				end
-				end
-				puts "\n"		
-				
-				#binding.pry
-							
-				@h[:examples].each do |x|
+			end
+			puts "\n"		
+			@h[:examples].each do |x|
 				if 	x[:status] == 'failed'
-					print 'F'.red	
+					@status << 'F'.red
+					@failed = @failed + 1	
 				end
 				if	x[:status] == 'passed'
-					print '.'.green	
+					@status << '.'.green	
+					@passed = @passed + 1	
 				end
 				if 	x[:status] == 'pending'
-					print 'P'.yellow	
-					end	
-				end			
+					@status << 'P'.yellow	
+					@pending = @pending + 1	
+				end	
+			end	
+		RSpec.clear_examples		
+		end
+			@status.each do |x|
+				print x
 			end
+			@totaltest = @failed + @passed + @pending
 			puts "\n"
-			puts "\n"+'Tests:'.green + @h[:summary][:example_count].to_s
-			puts 'Failed:'.red + @h[:summary][:failure_count].to_s
-			puts 'Pending:'.yellow + @h[:summary][:pending_count].to_s
+			puts "\n"+'Total tests: '.blue + @totaltest.to_s
+			puts 'Failed: '.red + @failed.to_s
+			puts 'Passed: '.green + @passed.to_s
+			puts 'Pending: '.yellow + @pending.to_s
 			puts "\n"+'Errors:'.red
 			puts @errors 
 			puts "\n"+'Duration:' + @h[:summary][:duration].to_s
