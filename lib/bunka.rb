2b5c6864fd4e958af0e0d06570a4d98c389cb0bf
Giel De Bleser <gieldebleser@gmail.com>
2015-04-08 14:44:07 +0200

    code clean up + file_expand nil if query

--- lib/bunka.rb
+++ lib/bunka.rb
@@ -15,7 +15,7 @@ class Bunka
       @threads = sequential ? 1 : threads
       @timeout_interval = timeout_interval
       @verbose_success = verbose_success
-      @file = File.expand_path(file)
+      @file ? File.expand_path(file) : nil
 
       Parallel.map(nodes, in_threads: @threads) do |fqdn|
         execute_query fqdn
@@ -32,7 +32,7 @@ class Bunka
       @processes = sequential ? 1 : processes
       @timeout_interval = timeout_interval
       @verbose_success = verbose_success
-      @file = File.expand_path(file)
+      @file ? File.expand_path(file) : nil
       @failedarray = []
       @successarray = []
       @timeoutarray = []
