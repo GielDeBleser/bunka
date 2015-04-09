44ec2224c6add97fe6ac126c47943a06368e305a
Giel De Bleser <gieldebleser@gmail.com>
2015-04-09 10:38:29 +0200

    File methode finished

--- lib/bunka.rb
+++ lib/bunka.rb
@@ -45,5 +45,22 @@ class Bunka
       socket_delete
       puts Time.now - start
     end
+
+    def testfile(path, query, timeout_interval, verbose_success, invert, sequential, threads, file = nil)
+      @command = "find . -name '#{path}' | egrep '.*'"
+      @invert = invert
+      @query = query
+      @sequential = sequential
+      @threads = sequential ? 1 : threads
+      @timeout_interval = timeout_interval
+      @verbose_success = verbose_success
+      @file = file ? File.expand_path(file) : nil
+
+      Parallel.map(nodes, in_threads: @threads) do |fqdn|
+        execute_query fqdn
+      end
+
+      print_summary
+    end
   end
 end
