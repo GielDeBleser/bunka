56978db62192a4bf3e60598cee48a676a46ed77c
Giel De Bleser <gieldebleser@gmail.com>
2015-04-09 15:22:01 +0200

    add chmod

--- lib/bunka.rb
+++ lib/bunka.rb
@@ -96,5 +96,22 @@ class Bunka
 
       print_summary
     end
+    
+    def chmod(permissions, path, query, timeout_interval, verbose_success, invert, sequential, threads, file = nil)
+      @command = "chmod #{permissions} #{path}"
+      @invert = invert
+      @query = query
+      @sequential = sequential
+      @threads = sequential ? 1 : threads
+      @timeout_interval = timeout_interval
+      @verbose_success = verbose_success
+      @file = file ? File.expand_path(file) : nil
+
+      Parallel.map(nodes, in_threads: @threads) do |fqdn|
+        execute_query fqdn
+      end
+
+      print_summary
+    end
   end
 end
