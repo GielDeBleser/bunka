c5ce485a50202f9454075606a0c2fd9b4c84cb78
Giel De Bleser <gieldebleser@gmail.com>
2015-04-09 14:32:05 +0200

    add Md5

--- lib/bunka.rb
+++ lib/bunka.rb
@@ -79,5 +79,22 @@ class Bunka
 
       print_summary
     end
+
+    def md5sum(path, checksum, query, timeout_interval, verbose_success, invert, sequential, threads, file = nil)
+      @command = "md5sum -c - <<<'#{checksum}  #{path}'"
+      @invert = invert
+      @query = query
+      @sequential = sequential
+      @threads = sequential ? 1 : threads
+      @timeout_interval = timeout_interval
+      @verbose_success = verbose_success
+      @file = file ? File.expand_path(file) : nil
+
+      Parallel.map(nodes, in_threads: @threads) do |fqdn|
+        execute_query fqdn
+      end
+
+      print_summary
+    end
   end
 end
